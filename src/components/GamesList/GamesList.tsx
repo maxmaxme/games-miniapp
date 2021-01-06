a53eaf89c47ea02f2a0e309609e5d87a530e75bb
Maxim Lepekha <m.lepekha@yandex.ru>
2021-01-06 23:25:47 +0300

    share and fav buttons

--- src/components/GamesList/GamesList.tsx
+++ src/components/GamesList/GamesList.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {GamesListItem} from '../GamesListItem/GamesListItem';
-import {CardGrid, Footer, Placeholder, Spinner} from '@vkontakte/vkui';
+import {Card, CardGrid, Footer, Placeholder, Spinner} from '@vkontakte/vkui';
 import {defaultProps, Game} from "../../utils/types";
 import {lang, langNumeric} from "../../utils/langs";
+import {Icon24ShareOutline, Icon24FavoriteOutline} from "@vkontakte/icons";
+import './GameList.css';
+import bridge from "@vkontakte/vk-bridge";
 
 interface Props extends defaultProps {
   games: Game[] | null;
@@ -18,13 +21,56 @@ export function GamesList(props: Props) {
   if (searchQuery.length > 0) {
     games = games.filter(game => game.title.toLowerCase().includes(searchQuery.toLowerCase()));
   }
-  const cardsSize = window.innerWidth > 666 ? 'm' : 'l' // todo заменить на platform(), когда там появится VKCOM
-  return <>
-    {
-      games.length > 0 ?
-      <CardGrid size={cardsSize}>{games.map((game, i) => <GamesListItem key={i} go={props.go} game={game} openModal={props.openModal}/>)}</CardGrid> :
-      <Placeholder>{lang('gamelist_search_not_found')}</Placeholder>
+  games = games.sort((a) => a.unavailable ? 1 : -1);
+
+  const twoCardsPerRow = window.innerWidth > 520 // todo заменить на platform(), когда там появится VKCOM
+  let gamesBlock = [];
+
+  const needShareButtons = true;
+
+  if (needShareButtons) {
+    if (games.length > 0) {
+      const firstRowGames = games.slice(0, twoCardsPerRow ? 2 : 1);
+      const otherRowsGames = games.slice(twoCardsPerRow ? 2 : 1);
+
+      gamesBlock.push(<CardGrid
+        size={twoCardsPerRow ? 'm' : 'l'}
+      >{firstRowGames.map((game, i) =>
+        <GamesListItem
+          key={i}
+          go={props.go}
+          game={game}
+          openModal={props.openModal}
+        />)}
+      </CardGrid>);
+
+      gamesBlock.push(<CardGrid size='m'>
+        <Card onClick={() => {
+          bridge.send("VKWebAppShare", {"link": "https://vk.com/app7718732"});
+        }} className="GamesList__subscribeBlock GamesList__subscribeBlock--green"><Icon24ShareOutline/>Поделиться</Card>
+        <Card onClick={() => {
+          bridge.send("VKWebAppAddToFavorites");
+        }} className="GamesList__subscribeBlock GamesList__subscribeBlock--orange"><Icon24FavoriteOutline/>В избранное</Card>
+      </CardGrid>);
+
+      gamesBlock.push(<CardGrid
+        size={twoCardsPerRow ? 'm' : 'l'}
+      >{otherRowsGames.map((game, i) =>
+        <GamesListItem
+          key={i}
+          go={props.go}
+          game={game}
+          openModal={props.openModal}
+        />)}
+      </CardGrid>);
+
     }
+  } else {
+    gamesBlock.push(<CardGrid size={twoCardsPerRow ? 'm' : 'l'}>{games.map((game, i) => <GamesListItem key={i} go={props.go} game={game} openModal={props.openModal}/>)}</CardGrid>);
+  }
+
+  return <>
+    {gamesBlock.length > 0 ? gamesBlock : <Placeholder>{lang('gamelist_search_not_found')}</Placeholder>}
     <Footer>{langNumeric(games.length, 'gamelist_search_count').replace('%s', String(games.length))}</Footer>
   </>;
 }
