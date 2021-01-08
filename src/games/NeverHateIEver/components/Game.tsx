247414661e4a3ac1d46d89d760470b3053820a94
Maxim Lepekha <m.lepekha@yandex.ru>
2021-01-08 14:16:32 +0300

    NeverHateIEver fixes

--- src/games/NeverHateIEver/components/Game.tsx
+++ src/games/NeverHateIEver/components/Game.tsx
@@ -3,6 +3,7 @@ import {Div, Placeholder} from "@vkontakte/vkui";
 import {GameButtons} from "./GameButtons";
 import {defaultProps} from "../../../utils/types";
 import {lang} from "../../../utils/langs";
+import {ReactSVG} from "react-svg";
 
 interface Props extends defaultProps {
   phrases: string[],
@@ -20,7 +21,10 @@ export const Game = (props: Props) => {
         <div className="NeverHateIEver__punishment">{punishment}</div> :
         <div className="NeverHateIEver__phrase">{phrase}</div>
       }
-    </Div> : <Placeholder stretched>{lang('games_neverihaveever_game_hint')}</Placeholder>}
+    </Div> : <Placeholder stretched>
+      {lang('games_neverihaveever_game_hint')}
+      <div className="NeverHateIEver__placeholderIcon"><ReactSVG src="/icons/down-chevron.svg" /></div>
+    </Placeholder>}
     <GameButtons
       go={go}
       openModal={openModal}
