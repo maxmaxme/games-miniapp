d291a6248c3d089593a6b60379124f97b7d56438
Maxim Lepekha <m.lepekha@yandex.ru>
2021-04-03 11:39:50 +0300

    routes

--- src/utils/panels.ts
+++ src/utils/panels.ts
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+import { Views } from './views';
+
 export function transformActivePanel(panel: string, defaultPanel: string, panels: object) {
   if (!Object.values(panels).includes(panel)) {
     panel = defaultPanel;
@@ -7,6 +9,8 @@ export function transformActivePanel(panel: string, defaultPanel: string, panels
 }
 
 export enum Panels {
+  GAMES_LIST = 'GAMES_LIST',
+
   NEVER_HATE_I_EVER_SETTINGS = 'NeverHateIEver_settings',
   NEVER_HATE_I_EVER_GAME = 'NeverHateIEver_game',
 
@@ -19,3 +23,29 @@ export enum Panels {
 
   OPEN_QUESTIONS_LIST = 'OpenQuestions_list',
 }
+
+export const routes = {
+  [Views.HOME]: [
+    Panels.GAMES_LIST,
+  ],
+
+  [Views.NEVER_HATE_I_EVER]: [
+    Panels.NEVER_HATE_I_EVER_GAME,
+    Panels.NEVER_HATE_I_EVER_SETTINGS,
+  ],
+
+  [Views.SPYFALL]: [
+    Panels.SPYFALL_GAME,
+    Panels.SPYFALL_SETTINGS,
+  ],
+
+  [Views.YES_OR_NO]: [
+    Panels.YES_OR_NO_INTRO,
+    Panels.YES_OR_NO_LIST_VIEW,
+    Panels.YES_OR_NO_ONE_VIEW,
+  ],
+
+  [Views.OPEN_QUESTIONS]: [
+    Panels.OPEN_QUESTIONS_LIST,
+  ],
+};
